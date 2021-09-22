b1a9cd0fec058e4c70ed4594515e805a33d8ed5c
Crowdbotics <team@crowdbotics.com>
2021-09-22 17:42:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "petite_craft_30751Identifier",
+  url: "https://petite_craft_30751Identifier.botics.co",
+  api: "https://petite_craft_30751Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
